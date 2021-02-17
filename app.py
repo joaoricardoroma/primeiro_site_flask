2eb67579b1c625236718c23c3c23df9a0cd90acd
Breno Valle <breno.valle9@hotmail.com>
2021-02-17 13:15:43 -0300

    criei o projeto home.html

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Home'
+    return render_template("home.html", messages=False)
 
 @app.route('/contato')
 def contact():
