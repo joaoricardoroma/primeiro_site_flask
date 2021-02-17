6cad55d7c55321cbe59b2c84fc669b440d0f7b0c
Breno Valle <breno.valle9@hotmail.com>
2021-02-17 16:39:39 -0300

    modifiquei o pokemon.html

--- app.py
+++ app.py
@@ -1,22 +1,26 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template("home.html", messages=False)
 
+
 @app.route('/contato')
 def contact():
     return 'contato'
+
 
 @app.route('/pokemons')
 def pokemons():
 
     return render_template("pokemons.html", data=[])
 
+
 @app.route('/pokemon/<int:id>')
 def pokemon(id):
     data = {
 
     }
-    return render_template("pokemon.html", data= data)
+    return render_template("pokemon.html", data=data)
