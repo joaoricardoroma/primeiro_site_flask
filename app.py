c1f764c3c94495d7d1b35ff049f3897b33f61747
brenininho <breno.valle9@hotmail.com>
2021-02-18 22:55:42 -0300

    apaguei uns comandos no app.py

--- app.py
+++ app.py
@@ -26,19 +26,7 @@ def pokemon(id):
     html_text = requests.get("https://www.pokemon.com/br/pokedex/bulbasaur").text
     soup = BeautifulSoup(html_text, "html.parser")
     name = soup.find("div", class_="pokedex-pokemon-pagination-title").text
-    # name1 = name.find("div").text
-    # number = name.find("span", class_="pokemon-number").text
-
     data = {
         "name": "bulbasaur"
     }
     return render_template("pokemon.html", data=data, name=name)
-
-
-html_text = requests.get("https://www.pokemon.com/br/pokedex/bulbasaur").text
-soup = BeautifulSoup(html_text, "html.parser")
-name = soup.find("div", class_="pokedex-pokemon-pagination-title").text
-# name1 = name.find("div").text
-# number = name.find("span", class_="pokemon-number").text
-# nome_do_pokemon = informacoes_do_pokemon.find("div")
-print(name)
